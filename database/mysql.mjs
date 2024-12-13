c1a9b578982564405eacc3f9dfe3abaa31a64136
CHCardosoNA <sikerajr68@gmail.com>
2024-12-13 07:51:42 -0300

    Update mysql.mjs

--- database/mysql.mjs
+++ database/mysql.mjs
@@ -1,10 +1,12 @@
 import { Sequelize } from "sequelize";
 
 const conexao = new Sequelize({
-    database: 'bdlpr2',
-    username: 'root',
-    password: 'root',
-    dialect: 'mysql'
+    host: 'dpg-cte109rtq21c73fvliqg-a',
+    port: '5432',
+    database: 'banco_claudio',
+    username: 'banco_claudio_user',
+    password: 'jhXZY4UvCYsRD8GbFtpq60IdI1bE3z7x',
+    dialect: 'postgree'
 });
 
 export default conexao;
